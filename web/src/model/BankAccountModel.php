e601644b8182019cb38039052552af339e984e74
Dylan Cross <dylancross@me.com>
2017-09-29 13:56:41 +1300

    Fixed jordans fuck up

--- web/src/model/BankAccountModel.php
+++ web/src/model/BankAccountModel.php
@@ -118,11 +118,9 @@ class BankAccountModel extends Model
      */
     public function save()
     {
-        $name = $this->_name??"NULL";
-        $balance = $this->_balance??0;
         if (!isset($this->_id)) {
             // New account - Perform INSERT
-            if (!$result = $this->db->query("INSERT INTO `bank_account` VALUES (NULL, '$name', $balance, $this->userID);")) {
+            if (!$result = $this->db->query("INSERT INTO `bank_account` VALUES (NULL,\"$this->_name\", $this->_balance, $this->_userID);")) {
                 // throw new ...
             }
             $this->_id = $this->db->insert_id;
